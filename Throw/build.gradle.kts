423d4e229440dfd47f3be4550f0d7e767d4c5658
Tim <timhdm@ya.ru>
2023-12-25 08:21:00 +0300

    first

---
+++ Throw/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "1.9.21"
+}
+
+group = "org.example"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(8)
+}
